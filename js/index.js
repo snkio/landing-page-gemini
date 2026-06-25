8b425fe2d6d160c86d0c31e56a82afceb54433b5
Viacheslav <snkio.dev@gmail.com>
2026-06-26 00:28:37 +0500

    add observer (anim in progress)

--- js/index.js
+++ js/index.js
@@ -5,3 +5,23 @@ setInterval(() => {
 }, 4000);
 
 // OBSERVER
+
+const section = document.querySelectorAll("[data-anim]");
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  },
+);
+
+section.forEach((e) => {
+  observer.observe(e);
+});
